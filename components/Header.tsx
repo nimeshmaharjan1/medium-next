1bbe585e222b2cf52536ebbb73a66b1ed00121c0
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-07-09 00:26:04 +0545

    iniial setuo

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Hello</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h1 className="text-9xl">Hello</h1>
+    </div>
+  );
+};
+
+export default index;
